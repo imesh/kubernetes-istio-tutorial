4e114f4da377181621667e4e7d0bd946a2f493a0
Imesh Gunaratne <imesh.gunaratne@gmail.com>
2022-09-15 14:40:23 +1000

    Populate order.invoice in order service getOrder()

--- order-service/main.go
+++ order-service/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"io"
 	"net/http"
-    "github.com/gin-gonic/gin"
 	"os"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
@@ -22,16 +26,22 @@ func getVersion(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, response)
 }
 
+type invoice struct {
+	InvoiceID string  `json:"invoice_id"`
+	Amount    float32 `json:"amount"`
+}
+
 type order struct {
-	ID string `json:"id"`
-	Date string `json:"date"`
-	Total float32 `json:"total"`
+	ID      string   `json:"id"`
+	Date    string   `json:"date"`
+	Total   float32  `json:"total"`
+	Invoice *invoice `json:"invoice"`
 }
 
 var orders = []order{
-	{ ID: "1", Date: "01-02-2022", Total: 1000.00},
-	{ ID: "2", Date: "02-02-2022", Total: 1500.00},
-	{ ID: "3", Date: "03-02-2022", Total: 2000.00},
+	{ID: "1", Date: "01-02-2022", Total: 1000.00},
+	{ID: "2", Date: "02-02-2022", Total: 1500.00},
+	{ID: "3", Date: "03-02-2022", Total: 2000.00},
 }
 
 func getOrders(c *gin.Context) {
@@ -44,12 +54,30 @@ func getOrder(c *gin.Context) {
 	for _, order := range orders {
 		if orderId == order.ID {
 			orderFound = true
+			invoice, err := getInvoice(orderId)
+			if err != nil {
+				panic(err)
+			} else {
+				order.Invoice = invoice
+			}
 			c.IndentedJSON(http.StatusOK, order)
 		}
 	}
-	if(!orderFound) {
+	if !orderFound {
 		c.AbortWithStatus(http.StatusNotFound)
 	}
+}
+
+func getInvoice(orderId string) (*invoice, error) {
+	resp, err := http.Get(fmt.Sprintf("http://invoice-service:8082/orders/%s/invoice", orderId))
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	invoice := invoice{}
+	json.Unmarshal([]byte(string(body)), &invoice)
+	return &invoice, nil
 }
 
 func postOrder(c *gin.Context) {
